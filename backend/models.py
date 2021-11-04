05b05332a7ecc7019ac6795ca52b16a796147927
Aizen Sōsuke <yohan@abehssera.com>
2021-11-04 15:18:33 +0100

    fix

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-def Session(models.Model):
+class Session(models.Model):
     sessionId = models.CharField(max_length = 100)
     ownerId = models.CharField(max_length = 100)
     collaboratorId = models.CharField(max_length = 100)
