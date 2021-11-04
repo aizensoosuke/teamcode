c82548a2267c33a1a83ced4ea3dc09a64f5245bc
Aizen Sōsuke <yohan@abehssera.com>
2021-11-04 15:21:21 +0100

    fix

--- backend/admin.py
+++ backend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from backend.models import Session
 
 # Register your models here.
+class SessionAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Session, SessionAdmin)
