cbc199848691b97e2192f96f0a7edbe8d7fc9ae3
Aizen Sōsuke <yohan@abehssera.com>
2021-11-04 15:17:06 +0100

    add Session model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+def Session(models.Model):
+    sessionId = models.CharField(max_length = 100)
+    ownerId = models.CharField(max_length = 100)
+    collaboratorId = models.CharField(max_length = 100)
